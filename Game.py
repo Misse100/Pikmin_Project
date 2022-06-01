7548985e9c01a621325f2046a6781173a4880821
Misse100 <lukashansgustafsson@gmail.com>
2022-06-01 21:16:41 +0200

    Finished the game

--- Game.py
+++ Game.py
@@ -1,15 +1,18 @@
 from cmu_graphics import *
 
 app.background = "darkBlue"
-app.stepsPerSecond = 30
+app.stepsPerSecond = 20
 app.movable = Group()
-map = Group(Circle(200,200,600,fill=gradient("darkGreen", "green","lightGreen","lightBlue")))
+map = Group(Circle(200,200,600,fill=gradient("darkGreen", "green","lightGreen","gold")))
 obstaclespawn = Circle(200,200,230,fill=None)
 ballspawn = Circle(200,200,130,fill=None)
-Explosion = Star(200,200,240,20,fill=gradient("yellow","orange","red","red"),visible=False)
+explosion = Group(Star(200,200,240,20,fill=gradient("yellow","orange","red","red")),Label("Bad luck",200,160,size=25))
+explosion.visible=False
 minimap = Rect(330,20,50,50,fill=None,border="black")
 icon = Rect(350,40,10,10)
-drowning = Rect(0,0,400,400,fill="blue",opacity=40,visible = False)
+drowning = Group(Rect(0,0,400,400,fill="blue",opacity=40),Label("Maybe try breathing air next time",200,160,size=25))
+drowning.visible = False
+Reset = Label("Press Space to go back to menu",200,300,size=20,visible = False)
 #player
 leftleg = Rect(202,205,3,12,rotateAngle=155)
 leftleg.a = 0.5
@@ -20,8 +23,9 @@ man = Group(Circle(200,190,5),body,Rect(207,190,3,12,rotateAngle=240),
 Rect(190,190,3,12,rotateAngle=120),leftleg,rightleg)
 #Labels
 timer = Label(5,200,30,size=25)
-Score = Label(0,200,330,size=20)
-winner = Label("Congratulations",200,160,size=30,visible = False)
+score = Label(0,200,330,size=20)
+winner = Group(Label("Congratulations",200,120,size=30),Star(70,70,50,15,fill=gradient("lime","yellow")),Star(340,70,50,15,fill=gradient("lime","yellow")))
+winner.visible = False
 #variables
 app.time = 900
 app.numballs = 3
@@ -38,8 +42,8 @@ app.started = False
 app.drowned = False
 app.win = False
 app.buttons = 0
-app.Obstacle = Group()
-app.movable.add(map,obstaclespawn,ballspawn,app.collectible,app.bullets,app.Obstacle)
+app.obstacle = Group()
+app.movable.add(map,obstaclespawn,ballspawn,app.collectible,app.bullets,app.obstacle)
 app.topY1 = -400
 app.botY1= 800
 app.leftX1 = -400
@@ -48,8 +52,14 @@ app.rightX1 = 200
 app.rightX2 = 800
 #menu
 blue = Rect(0,0,400,400,fill="lightblue")
-start = Rect(140,40,120,40,fill="orange",border="black")
-Menu = Group(blue, start)
+start = Rect(140,60,120,40,fill="orange",border="black")
+Menu = Group(blue, start,Label("START",200,80,size=25),Label("Disaster Survival",200,30,size=30,bold=True),
+Label("Use WASD to move across the island",200,260,size=20),Label("Use the ball people to turn off the bomb",200,300,size=20),
+Label("Shoot the switches with SPACE",200,340,size=20),Label("Try not to drown :)",200,380,size=18),
+Rect(302,205,3,12,rotateAngle=155),Rect(296,205,3,12,rotateAngle=215),
+Rect(297.5,195,5,12),Circle(300,190,5),Rect(307,190,3,12,rotateAngle=240),Rect(290,190,3,12,rotateAngle=120),
+Rect(80,190,20,20,fill="red",border="black"),Circle(200,202,5),
+Circle(198, 200,1,fill="white"),Circle(203, 200,1,fill="white"))
 def makeObstacle(difficulty,x,y):
     numrows = difficulty
     numcols = difficulty
@@ -57,7 +67,7 @@ def makeObstacle(difficulty,x,y):
     for row in range(numrows):
         for col in range(numcols):
             bruh = Rect(x + row * 25, y + col*25,20,20,fill="red",border="black")
-            app.Obstacle.add(bruh)
+            app.obstacle.add(bruh)
             app.buttons += 1
 def spawnObstacles():
     #left
@@ -139,7 +149,7 @@ def onMousePress(mouseX,mouseY):
     if app.started == False:
         if start.hits(mouseX,mouseY):
             app.started = True
-            Menu.clear()
+            Menu.visible = False
 def shoot(direction):
     if direction == "down":
         x = 200
@@ -230,15 +240,39 @@ def onKeyHold(keys):
             app.movable.centerY = 200
             app.numhearts -= 1
 def onKeyPress(key):
-    if app.gameOver == False:
+    if app.gameOver == False and app.drowned == False and app.win == False and app.started == True:
         if key == "space" and app.numballs > 0:
             shoot(app.direction)
-    
-
+    if app.gameOver == True or app.drowned == True or app.win == True:
+        if key == "space":
+            app.gameOver = False 
+            app.drowned = False
+            app.win = False 
+            app.started = False
+            Menu.visible = True
+            explosion.visible = False
+            man.rotateAngle = 0
+            drowning.visible = False
+            app.numballs = 3
+            app.numhearts = 3
+            app.collectible.clear()
+            spawnBalls(20)
+            app.obstacle.clear()
+            app.buttons = 0
+            spawnObstacles()
+            timer.visible = True
+            score.visible = True
+            app.time = 900
+            app.movable.centerX = 200
+            app.movable.centerY = 200
+            Reset.visible = False
+            winner.visible = False
 def onStep():
     if app.gameOver == False and app.drowned == False and app.win == False and app.started == True:
-        timer.value = "Island detonation in " + str(app.time // 30)
-        Score.value = "Turn off " + str(app.buttons) +" switches"
+        if app.buttons == 0:
+            app.win = True
+        timer.value = "Island detonation in " + str(app.time // 20)
+        score.value = "Turn off " + str(app.buttons) +" switches"
         if app.time < 1:
             app.gameOver = True
         else:
@@ -250,7 +284,7 @@ def onStep():
         app.hearts.clear()
         adjustHearts()
         for bul in app.bullets.children:
-            for obstacle in app.Obstacle.children:
+            for obstacle in app.obstacle.children:
                 if bul.hitsShape(obstacle) and obstacle.fill == "red":
                     app.bullets.remove(bul)
                     obstacle.fill = "lightgrey"
@@ -268,18 +302,22 @@ def onStep():
             if i.centerY > 400 or i.centerY < 0:
                 app.bullets.remove(i)
     elif app.gameOver == True:
-        Explosion.visible = True
+        explosion.visible = True
         man.rotateAngle = 90
-        Score.visible = False
+        score.visible = False
         timer.visible = False
+        Reset.visible = True
     elif app.drowned == True:
         man.rotateAngle = 180
         drowning.visible = True
-        Score.visible = False
+        score.visible = False
         timer.visible = False
+        Reset.visible = True
     elif app.win == True:
         winner.visible = True
-        Score.visible = False
+        score.visible = False
         timer.visible = False
+        Reset.visible = True
+
 
 cmu_graphics.run()
